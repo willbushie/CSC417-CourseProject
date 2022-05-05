6e16e720779628909ae7d706ad27204e02ac1d8d
William Bushie <willbushie@gmail.com>
2022-05-05 03:29:41 -0500

    playable game between a person and the AI

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ from player import player
 from bot_player import bot_player
 
 
-p1 = player("Luke",100)
-p2 = player("Bushie",100)
-p3 = player("player3",100)
+p1 = bot_player("Mr. Gonzalas",100)
+p2 = player("Litman",100)
 newGame = game([p1,p2],5,play=True)
-
-
-# use decision trees for this project
-# if the probability of winning is low, fold
-# if the probability of winning decreased, fold
-
