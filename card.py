a8bdd9980db5627daf48fa56dafac27ff5d6440c
William Bushie <willbushie@gmail.com>
2022-05-05 01:28:27 -0500

    creation of card.getInfoSimple() to be used during probability checking by bot_player

--- card.py
+++ card.py
@@ -23,3 +23,17 @@ class card:
         Return a string of the card rank.
         """
         return self.rank
+
+    def getInfoSimple(self):
+        """
+        This method returns a simple form of the cards rank and suit. `1c` is the One of Clubs. `Ad` is the Ace of Diamonds.
+        """
+        letter = ["One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+        number = ["1","2","3","4","5","6","7","8","9"]
+        simple = ""
+        if (self.getRank() in letter):
+            simple = simple + number[(letter.index(self.getRank()))]
+        elif (self.getRank() not in letter):
+            simple = simple + (self.getRank())[:1]
+        simple = simple + ((self.getSuit())[:1]).lower()
+        print(simple)
